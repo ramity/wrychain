9ee2b1fc31e3dc985fe0057afb518923b2bbb1c9
ramity <lewisclbrown@gmail.com>
2025-05-21 00:09:25 -0500

    added passthrough controller def for webpush node server

---
+++ src/symfony/src/Controller/WebPushController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\WebPushSubscription;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WebPushController extends AbstractController
+{
+    private HttpClientInterface $client;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
+    {
+        $this->client = $client;
+        $this->entityManager = $entityManager;
+    }
+
+    // Forward request to notification backend
+    #[Route('/subscribe', name: 'subscribe', methods: ['POST'])]
+    public function subscribe(Request $request): Response
+    {
+        $notification_backend_subscribe_endpoint = 'http://wrychain_node:3000/subscribe';
+
+        // Extract headers
+        $headers = [];
+        foreach ($request->headers->all() as $key => $values) {
+            $headers[$key] = implode(', ', $values);
+        }
+
+        // Forward request to notification backend
+        $response = $this->client->request('POST', $notification_backend_subscribe_endpoint, [
+            'body' => $request->getContent(),
+            'headers' => $headers
+        ]);
+
+        // Relay back the response
+        return $this->json($response);
+    }
+
+    #[Route('/send-notification', name: 'notification')]
+    public function send_notification(): Response
+    {
+        $notification_backend_subscribe_endpoint = 'http://wrychain_node:3000/send-notification';
+
+        // Forward request to notification backend
+        $response = $this->client->request('POST', $notification_backend_subscribe_endpoint);
+
+        // Relay back the response
+        return $this->json($response);
+    }
+}
