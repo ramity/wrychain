4cf5a3124fb629afb7ee93752044ebd9ea68c55d
ramity <lewisclbrown@gmail.com>
2025-05-22 22:39:52 -0500

    readded copy env script

---
+++ scripts/copy-env.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -u docker/mariadb/.env docker/mariadb/.env.local
+cp -u docker/nginx/wrychain.conf docker/nginx/wrychain.conf.local
+cp -u docker/nginx/example-ssl.conf docker/nginx/wrychain-ssl.conf.local
+cp -u docker/node/.env docker/node/.env.local
+cp -u docker/phpmyadmin/.env docker/phpmyadmin/.env.local
+cp -u docker/redis/redis-full.conf docker/redis/redis-full.conf.local
+cp -u docker/symfony/.env docker/symfony/.env.local
